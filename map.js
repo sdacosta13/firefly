079c364ad06fd81c1208ad3cd6b84c2dacc69227
j38067bl <benjamin.leadbetter@student.manchester.ac.uk>
2020-02-11 15:46:39 +0000

    Using watchPosition function to continuously update position

--- map.js
+++ map.js
@@ -1,11 +1,7 @@
 function initMap() {
   // Initialize and add the map
   var infoWindow;
-  var fogOverlay;
 
-  // The location of our markers
-  var kilburn = {lat: 53.467539, lng: -2.233927};
-  var building2 = {lat: 53.468634, lng: -2.235898};
   // The map, centered at the Kilburn building
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 17, maxZoom: 17, minZoom: 17, center: kilburn, disableDefaultUI: false,
@@ -17,8 +13,18 @@ function initMap() {
       fullscreenControl: false,});
   infoWindow = new google.maps.InfoWindow;
 
+  // The location of our markers
+  var kilburn = {lat: 53.467539, lng: -2.233927};
+  var building2 = {lat: 53.468634, lng: -2.235898};
+
+  // The markers, positioned at the buidlings we are using
+  var kilburnMarker = new google.maps.Marker({position: kilburn, map: map});
+  var marker2 = new google.maps.Marker({position: building2, map: map});
+
+  createFog(map);
+
   if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
+      navigator.geolocation.watchPosition(function(position) {
         var pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
@@ -34,7 +40,6 @@ function initMap() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
 
-
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
@@ -43,16 +48,16 @@ function initMap() {
     infoWindow.open(map);
   }
 
-  // The markers, positioned at the buidlings we are using
-  var kilburnMarker = new google.maps.Marker({position: kilburn, map: map});
-  var marker2 = new google.maps.Marker({position: building2, map: map});
+}
+
+function createFog(map) {
   var imageBounds = {
     north: 53.468755,
     south: 53.405814,
     east: -2.230045,
     west: -2.244038,
   };
-
+  var fogOverlay;
   fogOverlay = new google.maps.GroundOverlay(
     'https://lh4.googleusercontent.com/proxy/rpCG_YNAAsVgeen5Yz06GIBN6arMktQlgt-Z6-bSoZWcettRMFLefSTZ8XiLU2ENh0jo2ZZ_wmX97I_wkljdmAKfGJOqCxJC3tUTfelDVKSD3aTweMIyViPWEDY',
     imageBounds);
