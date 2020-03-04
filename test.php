e1cd788bf3997d32ac995449f5b46cb485cab2d3
Ben Leadbetter <leadbetter.benjamin@student.manchester.ac.uk>
2020-03-04 16:22:21 +0000

    Get array of latitudes and longitudes

--- test.php
+++ test.php
@@ -1,30 +1,27 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <?php
-      $y = 8;
-    ?>
-
-    <title>Firefly</title>
-    <script>
-      var y = '<?php echo $y ?>'
-    </script>
   </head>
   <body>
-    <button onclick="abc()">Try </button>
     <script>
-      function abc() {
-        alert(y);
-        document.getElementById("test").innerHTML = "Test";
-      }
-
       function reqListener () {
         console.log(this.responseText);
       }
 
       var oReq = new XMLHttpRequest();
       oReq.onload = function() {
-        alert(this.responseText);
+        var latlongsstr = this.responseText;
+
+        latlongsstr = latlongsstr.replace("[", "");
+        latlongsstr = latlongsstr.replace("]", "");
+        latlongsstr = latlongsstr.split("\"").join("");
+
+        var latlongs = latlongsstr.split(",");
+
+        for (i = 0; i < latlongs.length; i = i + 2) {
+          alert(latlongs[i]);
+          alert(latlongs[i+1]);
+        }
       };
       oReq.open("get", "getDataTest.php", true);
       oReq.send()
