a74b458643e3e93d3d0bbd7f4197ffb0c3dae18c
Ben Leadbetter <leadbetter.benjamin@student.manchester.ac.uk>
2020-03-04 17:29:37 +0000

    Get location data from database for map

--- map.js
+++ map.js
@@ -1,112 +1,136 @@
-currentFound = 0;
-function initMap() {
-  // Initialize and add the map
-  var infoWindow;
-  var person = null;
-  var pos;
+function reqListener () {
+  console.log(this.responseText);
+}
 
-  // The location of our markers
-  var kilburn = {lat: 53.467539, lng: -2.233927};
-  var museum = {lat: 53.466341, lng: -2.234195};
-  var union = {lat: 53.464374, lng: -2.232154};
-  var markerLocations = [kilburn, museum, union];
-  maxBuildings = markerLocations.length;
+var oReq = new XMLHttpRequest();
+oReq.onload = function() {
+  var latlongsstr = this.responseText;
 
-  // The description for each building
-  var kilburnDescription = "This is the Kilburn building, the building for Computer Science at the University of Manchester. \nThe building is named after Tom Kilburn, a famous mathematician and computer scienctist who worked on the world's first electronic stored-program computer, the Manchester Baby";
-  var museumDescription = "This is the Manchester Museum";
-  var unionDescription = "This is the student's union for the University of Manchester";
-  var descriptions = [kilburnDescription, museumDescription, unionDescription];
+  latlongsstr = latlongsstr.replace("[", "");
+  latlongsstr = latlongsstr.replace("]", "");
+  latlongsstr = latlongsstr.split("\"").join("");
 
-  // The map, centered at the Kilburn building
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 17, maxZoom: 17, minZoom: 17, center: kilburn, disableDefaultUI: false,
-      zoomControl: false,
-      mapTypeControl: false,
-      scaleControl: false,
-      streetViewControl: false,
-      rotateControl: false,
-      fullscreenControl: false,});
-  infoWindow = new google.maps.InfoWindow;
+  var latlongs = latlongsstr.split(",");
+  var markerLocations = [];
 
-  // The markers, positioned at the buidlings we are using
-  var kilburnMarker = new google.maps.Marker({position: kilburn, map: map});
-  var museumMarker = new google.maps.Marker({position: museum, map: map});
-  var unionMarker = new google.maps.Marker({position: union, map: map});
-  var markers = [kilburnMarker, museumMarker, unionMarker];
-
-  createFog(map);
-
-  if (navigator.geolocation) {
-      // Repeatedly gets the user's location
-      navigator.geolocation.watchPosition(function(position) {
-        pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-
-        // Set marker for user's location
-        if (person === null) {
-          person = new google.maps.Marker({position: pos, map: map});
-        } else {
-          person.setPosition(pos);
-        }
-
-        map.setCenter(pos);
-
-        // Check if user is near buildings
-        for (i = 0; i < markerLocations.length; i++) {
-          if ((Math.abs(pos.lat - markerLocations[i].lat) <= 0.001) && (Math.abs(pos.lng - markerLocations[i].lng) <= 0.01)) {
-            // Setting up the message for the user
-            var alertMessage = "You have found a building"
-            alertMessage = alertMessage.concat("\n", descriptions[i]);
-            alertMessage = alertMessage.concat("\n", "View your progress from the menu to find out more to find out more")
-
-            // Removing the marker
-            markers[i].setMap(null);
-
-            // Removing the building from the relevant arrays
-            markerLocations.splice(i, 1);
-            markers.splice(i, 1);
-            descriptions.splice(i, 1);
-
-            // Displaying the message to the user
-            alert(alertMessage);
-
-            // Updating the variable for number of variables the user has found
-            currentFound = currentFound + 1;
-          }
-        }
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
+  for (i = 0; i < latlongs.length; i = i + 2) {
+    temp = {lat: latlongs[i], lng: latlongs[i+1]};
+    markerLocations.push(temp);
   }
 
-}
+  initMap();
 
-// Creates the fog overlay on the map
-function createFog(map) {
-  var imageBounds = {
-    north: 53.477281,
-    south: 53.457807,
-    east: -2.218672,
-    west: -2.244703,
-  };
-  var fogOverlay;
-  fogOverlay = new google.maps.GroundOverlay(
-    'https://lh4.googleusercontent.com/proxy/rpCG_YNAAsVgeen5Yz06GIBN6arMktQlgt-Z6-bSoZWcettRMFLefSTZ8XiLU2ENh0jo2ZZ_wmX97I_wkljdmAKfGJOqCxJC3tUTfelDVKSD3aTweMIyViPWEDY',
-    imageBounds);
-    fogOverlay.setMap(map);
-    fogOverlay.setOpacity(0.5);
-}
+  function initMap() {
+    // Initialize and add the map
+    var infoWindow;
+    var person = null;
+    var pos;
+
+    // The location of our markers
+    var kilburn = {lat: 53.467539, lng: -2.233927};
+    var museum = {lat: 53.466341, lng: -2.234195};
+    var union = {lat: 53.464374, lng: -2.232154};
+    var markerLocations = [kilburn, museum, union];
+
+    // The description for each building
+    var kilburnDescription = "This is the Kilburn building, the building for Computer Science at the University of Manchester. \nThe building is named after Tom Kilburn, a famous mathematician and computer scienctist who worked on the world's first electronic stored-program computer, the Manchester Baby";
+    var museumDescription = "This is the Manchester Museum";
+    var unionDescription = "This is the student's union for the University of Manchester";
+    var descriptions = [kilburnDescription, museumDescription, unionDescription];
+
+    // The map, centered at the Kilburn building
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 17, maxZoom: 17, minZoom: 17, center: kilburn, disableDefaultUI: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: false,});
+    infoWindow = new google.maps.InfoWindow;
+
+    // The markers, positioned at the buidlings we are using
+    var kilburnMarker = new google.maps.Marker({position: kilburn, map: map});
+    var museumMarker = new google.maps.Marker({position: museum, map: map});
+    var unionMarker = new google.maps.Marker({position: union, map: map});
+    var markers = [kilburnMarker, museumMarker, unionMarker];
+
+    createFog(map);
+
+    if (navigator.geolocation) {
+        // Repeatedly gets the user's location
+        navigator.geolocation.watchPosition(function(position) {
+          pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          // Set marker for user's location
+          if (person === null) {
+            person = new google.maps.Marker({position: pos, map: map});
+          } else {
+            person.setPosition(pos);
+          }
+
+          map.setCenter(pos);
+
+          // Check if user is near buildings
+          for (i = 0; i < markerLocations.length; i++) {
+            if ((Math.abs(pos.lat - markerLocations[i].lat) <= 0.001) && (Math.abs(pos.lng - markerLocations[i].lng) <= 0.01)) {
+              // Setting up the message for the user
+              var alertMessage = "You have found a building"
+              alertMessage = alertMessage.concat("\n", descriptions[i]);
+              alertMessage = alertMessage.concat("\n", "View your progress from the menu to find out more to find out more")
+
+              // Removing the marker
+              markers[i].setMap(null);
+
+              // Removing the building from the relevant arrays
+              markerLocations.splice(i, 1);
+              markers.splice(i, 1);
+              descriptions.splice(i, 1);
+
+              // Displaying the message to the user
+              alert(alertMessage);
+
+              // Updating the variable for number of variables the user has found
+              currentFound = currentFound + 1;
+            }
+          }
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+    }
+
+  }
+
+  // Creates the fog overlay on the map
+  function createFog(map) {
+    var imageBounds = {
+      north: 53.477281,
+      south: 53.457807,
+      east: -2.218672,
+      west: -2.244703,
+    };
+    var fogOverlay;
+    fogOverlay = new google.maps.GroundOverlay(
+      'https://lh4.googleusercontent.com/proxy/rpCG_YNAAsVgeen5Yz06GIBN6arMktQlgt-Z6-bSoZWcettRMFLefSTZ8XiLU2ENh0jo2ZZ_wmX97I_wkljdmAKfGJOqCxJC3tUTfelDVKSD3aTweMIyViPWEDY',
+      imageBounds);
+      fogOverlay.setMap(map);
+      fogOverlay.setOpacity(0.5);
+  }
+
+};
+oReq.open("get", "getLatLong.php", true);
+oReq.send()
