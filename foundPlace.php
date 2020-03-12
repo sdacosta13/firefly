28afd1a55ea8f793916ca0392c9ef4a7a096808d
Ben Leadbetter <leadbetter.benjamin@student.manchester.ac.uk>
2020-03-12 20:42:21 +0000

    Hidden form for location and username to send to PHP file when place found

---
+++ foundPlace.php
@@ -0,0 +1,6 @@
+<?php
+  $latitude = $_POST['latitude'];
+  $longitude = $_POST['longitude'];
+  $username = $_POST['username'];
+  echo "<script>alert(" . $latitude . $longitude . $username . ")</script";
+?>
--- map.js
+++ map.js
@@ -120,6 +120,12 @@ function begin() {
               alertMessage = alertMessage.concat("\n", descriptions[i]);
               alertMessage = alertMessage.concat("\n", "View your progress from the menu to find out more to find out more")
 
+              // Setting the values to send to the database
+              document.getElementById('latitude').value = markerLocations[i].lat;
+              document.getElementById("longitude").value = markerLocations[i].lng;
+              document.getElementById("username").value = descriptions[i];
+              document.getElementById("sendtodb").submit();
+
               // Removing the marker
               markers[i].setMap(null);
 
