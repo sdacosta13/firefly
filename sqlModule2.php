4cda8e6d150cf79e65542e912ed5243c5d83429c
Ben Leadbetter <leadbetter.benjamin@student.manchester.ac.uk>
2020-03-19 11:45:19 +0000

    Alerting that a login is successfully created after registering

--- sqlModule2.php
+++ sqlModule2.php
@@ -128,7 +128,6 @@
 	    if(addUser($uname, $password, $email, $testMsgs, $mysqli) == True){
 	       echo("good");
 	       echo("<script type='text/javascript'>alert('Account successfully created\nLogin to continue');location.href='login.html';</script>");
-         header("Location: login.html");
       } else {
 		echo("fail");
 		echo("<script type='text/javascript'>alert('Username or password is incorrect');location.href='register.html';</script>");
